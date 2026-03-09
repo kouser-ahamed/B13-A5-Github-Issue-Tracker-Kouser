4a2fea513fd95270df709a12e697f99dffe6e1f7
Kouser Ahamed <ahamed15-5643@diu.edu.bd>
2026-03-09 11:23:21 +0600

    add username and password input IDs and implement sign-in functionality

---
+++ signin.js
@@ -0,0 +1,17 @@
+document.getElementById("submit").addEventListener("click", function () {
+  // Get the username and password values
+  const username = document.getElementById("username");
+  const userValue = username.value.toLowerCase();
+  // Get the password value
+  const password = document.getElementById("password");
+  const passwordValue = password.value.toLowerCase();
+
+  // check if the username and password are correct
+  if (userValue === "admin" && passwordValue === "admin123") {
+    alert("Loging successFull");
+    window.location.assign("./home/home.html");
+  } else {
+    alert("Loging Failed");
+    return;
+  }
+});
